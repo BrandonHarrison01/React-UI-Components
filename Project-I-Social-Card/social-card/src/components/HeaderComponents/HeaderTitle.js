82935287d7a0808c82a9a9c6f132707fe9731920
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-27 23:29:38 -0600

    WIP finished header section

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className='HeaderTitle'>
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool 27 May</p>
+        </div>
+    )
+}
+
+export default HeaderTitle
